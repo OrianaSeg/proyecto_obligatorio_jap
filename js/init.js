0f26b35d4d15e57f00649fd06dfa21d8ce5e9bf4
OrianaSeg <orianacsr@gmail.com>
2022-08-21 19:49:23 -0300

    cambios para entrega 1

--- js/init.js
+++ js/init.js
@@ -1,6 +1,6 @@
 const CATEGORIES_URL = "https://japceibal.github.io/emercado-api/cats/cat.json";
 const PUBLISH_PRODUCT_URL = "https://japceibal.github.io/emercado-api/sell/publish.json";
-const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/";
+const PRODUCTS_URL = "https://japceibal.github.io/emercado-api/cats_products/101.json";
 const PRODUCT_INFO_URL = "https://japceibal.github.io/emercado-api/products/";
 const PRODUCT_INFO_COMMENTS_URL = "https://japceibal.github.io/emercado-api/products_comments/";
 const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
@@ -16,26 +16,26 @@ let hideSpinner = function(){
 }
 
 let getJSONData = function(url){
-    let result = {};
-    showSpinner();
-    return fetch(url)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }else{
-        throw Error(response.statusText);
-      }
-    })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
-    })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
+  let result = {};
+  showSpinner();
+  return fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }else{
+      throw Error(response.statusText);
+    }
+  })
+  .then(function(response) {
+        result.status = 'ok';
+        result.data = response;
         hideSpinner();
         return result;
-    });
+  })
+  .catch(function(error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
+  });
 }
---
+++ js/login.js
@@ -0,0 +1,22 @@
+function loginSuccesful()
+{
+    document.getElementById("signinForm").addEventListener("submit", function(e)
+    {
+        (e).preventDefault();
+        let user = document.getElementById("email").value;
+        let password = document.getElementById("password").value;
+        sessionStorage.setItem("user", user);
+        sessionStorage.setItem("password", password);
+        sessionStorage.getItem("user");
+
+        if (user != null)
+        {
+            window.location.href ="index.html";
+        }
+    });   
+}
+
+document.addEventListener("DOMContentLoaded", function(e)
+{
+  loginSuccesful();
+});
