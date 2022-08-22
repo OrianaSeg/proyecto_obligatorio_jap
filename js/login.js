cdf52f316c16b71be24d5ff2a53131d7507cca9b
OrianaSeg <orianacsr@gmail.com>
2022-08-21 22:13:14 -0300

    corrección 2

--- js/login.js
+++ js/login.js
@@ -1,20 +1,15 @@
 function loginSuccesful() {
-    document.getElementById("signinForm").addEventListener("submit", function(e)
-    {
+    document.getElementById("signinForm").addEventListener("submit", function (e) {
         (e).preventDefault();
         let user = document.getElementById("email").value;
         let password = document.getElementById("password").value;
-        sessionStorage.setItem("user", user);
-        sessionStorage.setItem("password", password);
-        sessionStorage.getItem("user");
 
-        if (user != null)
-        {
-            window.location.href ="index.html";
+        if (user !== null && password !== null) {
+            window.location.href = "index.html";
         }
-    });   
+    });
 }
 
-document.addEventListener("DOMContentLoaded", function(e) {
-  loginSuccesful();
+document.addEventListener("DOMContentLoaded", function (e) {
+    loginSuccesful();
 });
